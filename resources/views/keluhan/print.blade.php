21efcc25d16c0e57b565c3a6b4f296c350ea9bcf
ffeyproject <100005694+ffeyproject@users.noreply.github.com>
2024-06-06 09:18:39 +0700

    Print Show Qa

--- resources/views/keluhan/print.blade.php
+++ resources/views/keluhan/print.blade.php
@@ -1,6 +1,34 @@
 <!DOCTYPE html>
 <html xmlns=''>
 
+<style>
+    table {
+        width: 100%;
+        border-collapse: collapse;
+    }
+
+    td {
+        padding: 5px;
+        vertical-align: top;
+    }
+
+    .label {
+        width: 25%;
+        font-size: 14px;
+        font-weight: bold;
+    }
+
+    .value {
+        width: 70%;
+        font-size: 14px;
+    }
+
+    .barcode {
+        width: 10%;
+        font-size: 6px;
+    }
+</style>
+
 <head>
     <meta http-equiv='Content-Type' content='text/html; charset=utf-8' />
 
@@ -10,50 +38,46 @@
             <td valign='top'>
                 <table width='100%' cellspacing='0' cellpadding='0'>
                     <tr>
-                        <td valign='top' width='90%' style='font-size:16px;'> <strong>PT. GAJAH ANGKASA
+                        <td valign='top' width='10%'>
+                            <img src="{{ public_path('dist/img/logo_gajah.png') }}" style="width: 60px; height: 70px;">
+                        </td>
+                        <td valign='top' width='70%' style='font-size:15px;'> <strong>PT. GAJAH ANGKASA
                                 PERKASA</strong><br />
-                            <strong>MARKETING DEPT</strong><br /><br /><br><br>
+                            <strong>Quality Assurance Dept</strong><br /><br /><br><br>
                         </td>
-                        <td valign='top' width='70%'>
+                        <td valign='top' width='40%'>
+                            <img src="{{ public_path('image/logo_qa.png') }}" style="width: 50px; height: 50px;">
                         </td>
-                        <td valign='top' width='50%' style='font-size:14px;'>GAP - FRM - MKT - 06<br />
+                        <td valign='top' width='55%' style='font-size:14px;'>GAP - FRM - QA - 22<br />
                         </td>
                     </tr>
                 </table>
-                <table width='100%' height='100' cellspacing='0' cellpadding='0'>
+                {{-- <table width='100%' height='100' cellspacing='0' cellpadding='0'>
                     <tr>
                         <td>
-                            <div align='center' style='font-size: 22px;font-weight: bold;'>FORM KELUHAN PELANGGAN</div>
-                            <br><br>
+                            <div align='center' style='font-size: 22px;font-weight: bold;'>FORM PENYELESAIAN KELUHAN
+                                PELANGGAN</div>
+                            <br>
+                        </td>
+                    </tr>
+                </table> --}}
+
+                <table width='100%' cellspacing='0' cellpadding='3' border='1' bordercolor='#CCCCCC'>
+                    <tr>
+                        <td align='center' width='100%' bordercolor='#ccc' style='font-size:22px;'>
+                            <strong>FORM PENYELESAIAN KELUHAN
+                                PELANGGAN
+                            </strong>
                         </td>
                     </tr>
                 </table>
-                <table width='100%' height='100' cellspacing='0' cellpadding='0'>
+
+                <table>
                     <tr>
-                        <td valign='top' width='25%' style='font-size:16px;'><strong>Nomer FKP</strong><br>
-                            <strong>Tanggal </strong><br>
-                            <strong>Nama Toko/Buyer </strong><br>
-                            <strong>Nama Marketing </strong><br>
-                            <strong>Nomer Wo </strong><br>
-                            <strong>No Sc </strong><br>
-                            <strong>Nama Motif </strong><br>
-                            <strong>CW/,QTY </strong><br>
-                            <strong>Jenis </strong><br>
-                        </td>
-                        <td valign='top' width='70%' style='font-size:16px;'>
-                            : {{ $keluhan->nomer_keluhan }} <br>
-                            : {{ $keluhan->tgl_keluhan }} <br>
-                            : {{ $keluhan->buyer->nama_buyer }} <br>
-                            : {{ $keluhan->nama_marketing }} <br>
-                            : {{ $keluhan->no_wo }} <br>
-                            : {{ $keluhan->no_sc }} <br>
-                            : {{ $keluhan->nama_motif }} <br>
-                            : {{ $keluhan->cw_qty }}, {{ $keluhan->qty_complaint }} <br>
-                            : {{ $keluhan->jenis }} <br>
-                        </td>
-                        <td valign='top' width='10%' style='font-size:6px;'>
-                            {!!
-                            DNS2D::getBarcodeHTML(
+                        <td class="label">Nomer FKP</td>
+                        <td class="value">: {{ $keluhan->nomer_keluhan }}</td>
+                        <td class="barcode" rowspan="9">
+                            {!! DNS2D::getBarcodeHTML(
                             $keluhan->nomer_keluhan.'-'.
                             $keluhan->buyer->nama_buyer.'-'.
                             $keluhan->nama_marketing.'-'.
@@ -63,11 +87,50 @@
                             $keluhan->jenis.'-'.
                             $keluhan->masalah.'-'.
                             $keluhan->status
-                            ,'QRCODE',3,3)
-                            !!}<br /><br>
+                            ,'QRCODE',3,3) !!}<br /><br>
                         </td>
                     </tr>
+                    <tr>
+                        <td class="label">Tanggal FKP</td>
+                        <td class="value">: {{ $keluhan->tgl_keluhan }}</td>
+                    </tr>
+                    <tr>
+                        <td class="label">Nama Buyer</td>
+                        <td class="value">: {{ $keluhan->buyer->nama_buyer }}</td>
+                    </tr>
+                    <tr>
+                        <td class="label">Nama Marketing</td>
+                        <td class="value">: {{ $keluhan->nama_marketing }}</td>
+                    </tr>
+                    <tr>
+                        <td class="label">Nomer WO</td>
+                        <td class="value">: {{ $keluhan->no_wo }}</td>
+                    </tr>
+                    <tr>
+                        <td class="label">No Sc</td>
+                        <td class="value">: {{ $keluhan->no_sc }}</td>
+                    </tr>
+                    <tr>
+                        <td class="label">Nama Motif</td>
+                        <td class="value">: {{ $keluhan->nama_motif }}</td>
+                    </tr>
+                    <tr>
+                        <td class="label">CW/QTY</td>
+                        <td class="value">: {{ $keluhan->cw_qty }}, {{ $keluhan->qty_complaint }}</td>
+                    </tr>
+                    <tr>
+                        <td class="label">Jenis</td>
+                        <td class="value">: {{ $keluhan->jenis }}</td>
+                    </tr>
                 </table>
+                <table width='100%' cellspacing='0' cellpadding='3' border='1' bordercolor='#CCCCCC'>
+                    <tr>
+                        <td width='100%' bordercolor='#ccc' style='font-size:14px;'>
+                            <strong>ASAL MASALAH COMPLAINT
+                            </strong> :
+                        </td>
+                    </tr>
+                </table><br>
                 <table width='100%' cellspacing='0' cellpadding='3' border='1' bordercolor='#CCCCCC'>
                     <tr>
                         <td align="center" width='100%' bordercolor='#ccc' bgcolor='#f2f2f2' style='font-size:16px;'>
@@ -80,36 +143,100 @@
                         <td colspan="*">
                     <tr>
                         <td valign='top' style='font-size:12px;'>{!! $keluhan->masalah !!}</td>
+                </table><br>
+                <table width='100%' cellspacing='0' cellpadding='3' border='1' bordercolor='#CCCCCC'>
+                    <tr>
+                        <td align="center" width='100%' bordercolor='#ccc' bgcolor='#f2f2f2' style='font-size:16px;'>
+                            <strong>PENYEBAB MASALAH KOMPLAIN
+                            </strong>
+                        </td>
+                    </tr>
+                    <tr style="display:none;">
+                        <td colspan="*">
+                    </tr>
+                    <tr>
+                        <td valign='top' style='font-size:12px;' width="200" height="100">
+
+                        </td>
+                    </tr>
+                </table><br>
+                <table width='100%' cellspacing='0' cellpadding='3' border='1' bordercolor='#CCCCCC'>
+                    <tr>
+
+                        <td align="center" width='100%' bordercolor='#ccc' bgcolor='#f2f2f2' style='font-size:16px;'>
+                            <strong>TINDAKAN PERBAIKAN DAN PENCEGAHAN DARI PIHAK TERKAIT
+                            </strong>
+                        </td>
+                    </tr>
+                    <tr style="display:none;">
+                        <td colspan="*">
+                    </tr>
+                    <tr>
+                        <td valign='top' style='font-size:12px;' width="200" height="100"></td>
+                    </tr>
+                </table><br>
+                <table width='100%' cellspacing='0' cellpadding='8' border='1' bordercolor='#CCCCCC'>
+                    <tr>
+
+                        <td align="center" width='100%' bordercolor='#ccc' bgcolor='#f2f2f2' style='font-size:16px;'>
+                            <strong>TARGET WAKTU DAN HASIL VERIFIKASI
+                            </strong>
+                        </td>
+                    </tr>
+                    <tr style="display:none;">
+                        <td colspan="*">
+                    </tr>
+                    <tr>
+                        <td valign='top' style='font-size:14px;'>
+                            <?php
+                            // Misalkan variabel $keluhan->tgl_keluhan adalah string tanggal
+                            $tgl_keluhan = $keluhan->tgl_keluhan;
+
+                            // Mengubah string tanggal menjadi objek DateTime
+                            $date = new DateTime($tgl_keluhan);
+
+                            // Menambahkan 14 hari
+                            $date->modify('+16 days');
+
+                            // Mengubah kembali objek DateTime menjadi string tanggal
+                            $tgl_keluhan_baru = $date->format('Y-m-d');
+
+                            ?>
+                            <strong>Target Waktu : {{ \Carbon\Carbon::parse($tgl_keluhan_baru)->isoFormat('dddd, D MMMM
+                                Y')
+                                }}</strong><br>
+                            <strong>Tanggal Verifikasi : </strong><br>
+                            <strong>Hasil Verifikasi : </strong><br>
+                        </td>
+                    </tr>
+                </table><br>
+
+                <table width='100%' height='100' cellspacing='0' cellpadding='0'>
+                    <tr>
+                        <td valign='top' width='70%' style='font-size:14px;'>
+                            <strong>Team Penyelidik </strong><br><br>
+                            <br>
+                            ___________________<br>
+                        </td>
+                        <td valign='top' width='80%' style='font-size:14px;'>
+                            <strong>Kabag Terkait </strong><br><br>
+                            <br>
+                            ___________________<br>
+                        </td>
+                        <td valign='top' width='40%' style='font-size:14px;'>
+                            <strong>Manager Terkait </strong><br><br>
+
+                            <br>
+                            ___________________<br>
+                        </td>
+                    </tr>
+                </table>
+                <br>
+
             </td>
         </tr>
     </table>
 
-    <table class="table table-head-fixed text-nowrap">
-        <thead>
-            <tr>
-                <th>No</th>
-                <th>Gambar</th>
-                <th>Keterangan</th>
-            </tr>
-        </thead>
-        <?php $no = 1 ?>
-        <tbody>
-            @forelse ($icomplaint as $item)
-            <tr>
-                <td>{{ $no++ }}</td>
-                <td><img src="{{ url('image/keluhan/'.$item->nama_image) }}" style="width: 300px; height: 300px;">
-                </td>
-                <td>{!! $item->keterangan !!}</td>
-                @empty
-            <tr>
-                <td colspan="12">Gambar Pendukung Belum Ada.</td>
-            </tr>
-            </tr>
-        </tbody>
-        @endforelse
-    </table>
-    </td>
-    </table>
 </body>
 
 </html>
